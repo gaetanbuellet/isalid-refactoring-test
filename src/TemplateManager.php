b13403b4bea0f2e7cdcba0f7acf137b2bcd9955f
Gaetan Buellet <gaetan@dedi-agency.com>
2021-04-13 19:24:39 +0200

    refacto: remove usell condition before str_replace
    If the pattern is not present, str_replace will just not replace it

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -70,27 +70,19 @@ class TemplateManager
                 $destination = $this->destinationRepository->getById($quote->destinationId);
             }
 
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
+            $text = str_replace(
+                '[quote:summary_html]',
+                Quote::renderHtml($_quoteFromRepository),
+                $text
+            );
 
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
-            }
+            $text = str_replace(
+                '[quote:summary]',
+                Quote::renderText($_quoteFromRepository),
+                $text
+            );
 
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
+            $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
         }
 
         if (isset($destination))
